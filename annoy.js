da107c91d22112dfb8249f6c0fd4ad2ac451ec67
oss84 <36839952+oss84@users.noreply.github.com>
2018-04-12 11:29:55 +0300

    Add files via upload

---
+++ annoy.js
@@ -0,0 +1,9 @@
+//alert("Connected!");
+
+var answer = prompt("Are we there yet?");
+
+while(answer.indexOf("yes")===-1) {
+	var answer = prompt("Are we there yet?");
+}
+
+alert("Yeah, we made it!!!!");
